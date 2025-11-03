499ac52ca07c25c69ca488e0844610fe6259bc23
Edgar Marroquin <emarro@cs.cornell.edu>
2025-11-03 16:09:17 -0500

    Add checks to make sure zero-shot AR has correct inputs

--- main.py
+++ main.py
@@ -81,14 +81,30 @@ class ComposerWrapper(HuggingFaceModel):
             ref_bp_id = batch["ref_id"]  # [B]
             ref_ids = batch["input_ids"].detach().clone()
             B, seq_len = ref_ids.shape
+            var_idx = seq_len // 2
             # update (possibly masked) token with ref
-            ref_ids[:, (seq_len // 2) - 1] = ref_bp_id
+            ref_ids[:, var_idx] = ref_bp_id
 
+            assert torch.all(ref_ids[:, var_idx] == ref_bp_id), (
+                f"REF bps from batch do not match the input ids, IDS: {ref_ids[:, var_idx - 1 : var_idx + 2]}, ref_bps: {ref_bp_id}"
+            )
             alt_bp_id = batch["alt_id"]  # [B]
             alt_ids = batch["input_ids"].detach().clone()
             B, seq_len = alt_ids.shape
             # update (possibly masked) token with alt
-            alt_ids[:, (seq_len // 2) - 1] = alt_bp_id
+            alt_ids[:, var_idx] = alt_bp_id
+            assert torch.all(ref_bp_id != alt_bp_id), (
+                f"REF and ALT bps are the same bp for some batches. REF: {ref_bp_id[ref_bp_id == alt_bp_id]} ALT: {alt_bp_id[ref_bp_id == alt_bp_id]}"
+            )
+            assert torch.all(ref_ids[:, :var_idx] == alt_ids[:, :var_idx]), (
+                "Not all ids before the variant site match"
+            )
+            assert torch.all(ref_ids[:, var_idx + 1 :] == alt_ids[:, var_idx + 1 :]), (
+                "Not all ids after the variant site match"
+            )
+            assert torch.all(ref_ids[:, var_idx] != alt_ids[:, var_idx]), (
+                f"Some variants have the same ALT and REF bp, REF: {ref_ids[:, var_idx][ref_ids[:, var_idx] == alt_ids[:, var_idx]]} ALT: {alt_ids[:, var_idx][ref_ids[:, var_idx] == alt_ids[:, var_idx]]}"
+            )
 
             alt_outputs = self.model(input_ids=alt_ids)
 
@@ -146,7 +162,7 @@ class ComposerWrapper(HuggingFaceModel):
                 metric.update(value=val)
             return
         probs = outputs.logits.softmax(dim=-1)
-        batch_size, seq_len, vocab_len = outputs.shape
+        batch_size, seq_len, vocab_len = outputs.logits.shape
 
         if self.mlm:
             ref_bp = batch["ref_id"]  # [batch_size]
@@ -173,24 +189,37 @@ class ComposerWrapper(HuggingFaceModel):
             )
             score = torch.log(alt_prob / ref_prob)
         else:
-            alt_probs = outputs.alt_probs.logits.softmax(dim=-1)
+            alt_probs = outputs.alt_outputs.logits.softmax(dim=-1)
             ref_bp = batch["ref_id"]  # [B]
             alt_bp = batch["alt_id"]  # [B]
             input_ids = batch["input_ids"]  # [B, L]
+            var_idx = seq_len // 2
+
+            assert torch.all(input_ids[:, var_idx] == ref_bp), (
+                f"REF bps from batch do not match the input ids, IDS: {input_ids[:, var_idx - 1 : var_idx + 2]}, ref_bps: {ref_bp}"
+            )
+            assert torch.all(ref_bp != alt_bp), (
+                "Not all REF and  ALT bps are different, error in pre-processing"
+            )
+
             ref_log_probs = torch.log(
                 torch.gather(probs, dim=-1, index=input_ids.unsqueeze(-1)).squeeze(2)
             )
             alt_ids = input_ids.detach().clone()  # [B, L]
-            alt_ids[:, (seq_len // 2) - 1] = alt_bp
-            assert all(
-                input_ids[:, : (seq_len // 2) - 1] == alt_ids[:, : (seq_len // 2) - 1]
-            ), "Not all ids before the variant site match"
-            assert all(
-                input_ids[:, (seq_len // 2) + 1 :] == alt_ids[:, : (seq_len // 2) + 1 :]
+            alt_ids[:, var_idx] = alt_bp
+            assert torch.all(input_ids[:, :var_idx] == alt_ids[:, :var_idx]), (
+                "Not all ids before the variant site match"
+            )
+            assert torch.all(
+                input_ids[:, var_idx + 1 :] == alt_ids[:, var_idx + 1 :]
             ), "Not all ids after the variant site match"
-            assert all(
-                input_ids[:, (seq_len // 2) - 1] != alt_ids[:, (seq_len // 2) - 1]
-            ), "Some REF and ALT sequences have the same BP at the variant site"
+            ref_check = input_ids[:, var_idx]
+            alt_check = alt_ids[:, var_idx]
+            not_match = ref_check != alt_check
+
+            assert torch.all(not_match), (
+                f"Some REF and ALT sequences have the same BP at the variant site, REFs: {ref_check[~not_match]} ({ref_check[~not_match].shape}), ALTs: {alt_check[~not_match]} ({alt_check[~not_match].shape})"
+            )
 
             alt_log_probs = torch.log(
                 torch.gather(alt_probs, dim=-1, index=alt_ids.unsqueeze(-1)).squeeze(2)
@@ -367,14 +396,18 @@ def build_dataloader(
                 assert item["ref"] == seq_bp, (
                     f"Masking in eval dataloader failed, found {seq_bp} when we expected {item['ref']} around {sequence[var_idx - 5 : var_idx + 5]}"
                 )
+                assert item["alt"] != item["ref"], (
+                    f"Error, found REF bp {item['ref']} and ALT bp {item['alt']} to be the same"
+                )
                 # sequence[var_idx] = int(
                 #    self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
                 # )
-                sequence = (
-                    sequence[:var_idx]
-                    + self.tokenizer.mask_token
-                    + sequence[var_idx + 1 :]
-                )
+                if self.mlm:
+                    sequence = (
+                        sequence[:var_idx]
+                        + self.tokenizer.mask_token
+                        + sequence[var_idx + 1 :]
+                    )
                 ref_id = tokenizer(item["ref"], return_tensors="pt")["input_ids"][:, 0]
                 alt_id = tokenizer(item["alt"], return_tensors="pt")["input_ids"][:, 0]
 
