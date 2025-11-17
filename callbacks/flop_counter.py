28fa0c0ff796b757bfcb030a72da569711226fca
Edgar Marroquin <emarro@cs.cornell.edu>
2025-11-16 22:14:55 -0500

    Add basic bpic stats callback

--- callbacks/flop_counter.py
+++ callbacks/flop_counter.py
@@ -69,12 +69,20 @@ class FlopMonitor(Callback):
 
 
 def calc_compression_ratio(num_tokens, mask):
+    """
+    Calculcate the compression (number of tokens aggregated into a chunk)
+    returns:
+        the percentage of the sequence that was compressed
+        0 = no compression of the sequence
+        ~1 = nearly all of the sequence was compressed into a single token
+    """
     num_chunks = mask.to(torch.int).sum(dim=-1)  # [B]
-    return num_chunks / num_tokens  # [n]
+    return 1.0 - (num_chunks / num_tokens)  # [B]
 
 
 def calc_bpic(mask):
     """
+    Reference implementation, assumes unpacked ([B*L]) input
     Calculates the size of each inner chunk in [mask]
     Assumes that each chunk ends at every True in mask (including the True token)
     e.g. [1,0,1,1] are chunks of size [1, 2, 1]
@@ -98,12 +106,48 @@ def calc_bpic(mask):
             (intermediate[0:1], (intermediate[1:] - intermediate[:-1]) + 1)
         )
         results.append(result)
-    return result
+    return torch.cat(results, dim=0)  # [num_chunks]
+
+
+def calc_bpic_effecient(mask):
+    """
+    Calculates the size of each inner chunk in [mask]
+    Assumes that each chunk ends at every True in mask (including the True token)
+    e.g. [1,0,1,1] are chunks of size [1, 2, 1]
+    Args:
+      mask: torch.Tensor[bool]: [B, L] - the mask defining boundaries of inner chunks
+    Returns:
+      a tensor of size [num_chunks] (num_chunks = mask.sum()) containing the size of each chunk
+    """
+    # TODO: calc chunks stats per seq (doable given L)
+    if len(mask.shape) == 1:
+        mask = mask.unsqueeze(0)
+    B, L = mask.shape
+    mask[:, L - 1] = True  # always force last tok to be a boundary if not already
+    mask = mask.reshape(-1)  # [B * L]
+    intermediate = (
+        torch.cumsum((~mask).to(torch.int), dim=0) + 1
+    )  # Size of each chunk is at boundaries
+    only_cum_size = intermediate * mask  # mask out everything except total chunk size
+    size_mask = only_cum_size > 0
+    # NOTE: without knowing next_num_tokens here, we no longer know chunk size per seq
+    cum_sizes = torch.cat(
+        [torch.tensor([1], device=mask.device), only_cum_size[size_mask]], dim=0
+    )  # remove all non chunk sizes, left pad for next step
+    chunk_sizes = cum_sizes[1:] - cum_sizes[:-1] + 1
+    assert chunk_sizes.sum() == B * L, (
+        f"Chunks do not add up to num of input tokens {B * L}, only counted {chunk_sizes.sum()} tokens in input"
+    )
+    return chunk_sizes  # [num_chunks]
 
 
 class BPredMonitor(Callback):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args: Any, log_every: int = 10, **kwargs: Any) -> None:
+        super().__init__(*args, log_every=100, **kwargs)
+        self.history_bpic: deque[torch.Tensor] = deque(maxlen=log_every + 1)
+        self.history_eval_bpic: list[torch.Tensor] = list()
+        self.history_eval_cr: list[torch.Tensor] = list()
+        self.in_train: bool = True
 
     def batch_end(self, state: State, logger: Logger) -> None:
         # print(state.outputs)
@@ -116,18 +160,82 @@ class BPredMonitor(Callback):
         # print(mask)
         # print(mask.shape)
         num_tokens = torch.tensor([L] * B, device=mask.device, dtype=torch.long)
-        compression_ratio = calc_compression_ratio(num_tokens=num_tokens, mask=mask)
-        bpics = calc_bpic(mask)
-        max_bpics = [x.max().item() for x in bpics]
-        min_bpics = [x.min().item() for x in bpics]
-        mean_bpics = [x.float().mean().item() for x in bpics]
+        compression_ratio = calc_compression_ratio(
+            num_tokens=num_tokens, mask=mask
+        ).float()
+        bpics = calc_bpic_effecient(mask).detach().clone()
+        max_bpics = bpics.max()  # [x.max().item() for x in bpics]
+        min_bpics = bpics.min()  # [x.min().item() for x in bpics]
+        mean_bpics = bpics.float().mean()  # [x.float().mean().item() for x in bpics]
         log = {
-            "Boundaries/Compression ratio": compression_ratio.float().mean().item(),
-            "Boundaries/Max BPIC in batch": max(max_bpics),
-            "Boundaries/Min BPIC in batch": min(min_bpics),
-            "Boundaries/Mean BPIC in batch": sum(mean_bpics) / len(mean_bpics),
+            "Boundaries/Mean Seq Perc chunked in batch (1=Single Big Chunk)": compression_ratio.mean().item(),
+            "Boundaries/Max Seq Perc chunked in batch": compression_ratio.max().item(),
+            "Boundaries/Min Seq Perc chunked in batch": compression_ratio.min().item(),
+            "Boundaries/Max BPIC in batch": max_bpics.item(),
+            "Boundaries/Min BPIC in batch": min_bpics.item(),
+            "Boundaries/Mean BPIC in batch": mean_bpics.item(),
         }
         for k, v in log.items():
             print(f"{k}: {v:3f}")
             # TODO: change naming in logger
         logger.log_metrics(log)
+        self.history_bpic.append(bpics)
+        print(f"BPIC history of len {len(self.history_bpic)}: {self.history_bpic}")
+        if len(self.history_bpic) == self.history_bpic.maxlen:
+            full_bpics = torch.cat(list(self.history_bpic), dim=0)  # [num_total_chunks]
+            log = {
+                "Boundaries/Mean Seq Perc chunked windowed (1=Single Big Chunk)": compression_ratio.mean().item(),
+                "Boundaries/Max Seq Perc chunked windowed": compression_ratio.max().item(),
+                "Boundaries/Min Seq Perc chunked windowed": compression_ratio.min().item(),
+                "Boundaries/Max BPIC windowed": full_bpics.max().item(),
+                "Boundaries/Min BPIC windowed": full_bpics.min().item(),
+                "Boundaries/Mean BPIC windowed": full_bpics.float().mean().item(),
+            }
+            for k, v in log.items():
+                print(f"{k}: {v:3f}")
+
+    def eval_start(self, state: State, logger: Logger):
+        self.in_train = False
+        self.history_eval_bpic = list()
+        self.history_eval_cr = list()
+
+    def eval_batch_end(self, state: State, logger: Logger):
+        print(state.batch)
+        # TODO: parse out assembly, chrom, start, and end
+        B, L, _ = state.outputs.logits.shape
+        bpred_out = state.outputs.bpred_output  # list of bpred outputs
+        # TODO: remove reshape, it's slow AF but using it to visualize indiv batches
+        mask = bpred_out[0].boundary_mask.reshape(B, L)  # [B, L]
+        num_tokens = torch.tensor([L] * B, device=mask.device, dtype=torch.long)
+        compression_ratio = (
+            calc_compression_ratio(num_tokens=num_tokens, mask=mask)
+            .float()
+            .detach()
+            .clone()
+        )  # [B]
+        bpics = calc_bpic_effecient(mask).detach().clone()
+        self.history_eval_cr.append(compression_ratio)
+        self.history_eval_bpic.append(bpics)
+
+    def eval_end(self, state: State, logger: Logger):
+        self.in_train = True
+        compression_ratios = torch.cat(self.history_eval_cr, dim=0)  # [num_seqs]
+        bpics = torch.cat(self.history_eval_bpic, dim=0)  # [num_chunks]
+        log = {
+            "Eval Boundaries/Mean Seq Perc chunked (1=Single Big Chunk)": compression_ratios.float()
+            .mean()
+            .item(),
+            "Eval Boundaries/Max Seq Perc": compression_ratios.max().item(),
+            "Eval Boundaries/Min Seq Perc": compression_ratios.min().item(),
+            "Eval Boundaries/Max BPIC": bpics.max().item(),
+            "Eval Boundaries/Min BPIC": bpics.min().item(),
+            "Eval Boundaries/Mean BPIC": bpics.float().mean().item(),
+        }
+        logger.log_metrics(log)
+        print(
+            f"Eval metrics on {bpics.size(0)} chunks, L=511 equals to {bpics.sum().item() // 511:,} sequences"
+        )
+        for k, v in log.items():
+            print(f"{k}: {v:3f}")
+        # TODO: Log a histogram of the compression ratios and off the bpics
+        # TODO: Log images of the boundaries (maybe better left to a different callback or dedicated Evaluator & datasset?)
