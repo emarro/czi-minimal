b9cd9c33c94338d56061841bf12aa523b2817e58
Edgar Marroquin <emarro@cs.cornell.edu>
2026-01-05 16:27:35 -0500

    update init

--- src/caduceus/caduceus/__init__.py
+++ src/caduceus/caduceus/__init__.py
@@ -1,6 +1,19 @@
-"""Hugging Face config, model, and tokenizer for Caduceus.
+"""Hugging Face config, model, and tokenizer for Caduceus."""
 
-"""
 from .configuration_caduceus import CaduceusConfig
-from .modeling_caduceus import Caduceus, CaduceusForMaskedLM, CaduceusForSequenceClassification
+from .modeling_caduceus import (
+    Caduceus,
+    CaduceusForMaskedLM,
+    CaduceusForSequenceClassification,
+    BiMambaWrapper,
+)
 from .tokenization_caduceus import CaduceusTokenizer
+
+# __all__ = [
+#    "CaduceusConfig",
+#    "Caduceus",
+#    "CaduceusForMaskedLM",
+#    "CaduceusForSequenceClassification",
+#    "CaduceusTokenizer",
+#    "BiMambaWrapper",
+# ]
