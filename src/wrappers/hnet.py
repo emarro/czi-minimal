068de6aec57c2cd73eca3e4b00ef2a56a432bfe8
Edgar Marroquin <emarro@cs.cornell.edu>
2026-04-26 15:41:28 -0400

    switch tokenizers between bytes and DNA"

--- src/wrappers/hnet.py
+++ src/wrappers/hnet.py
@@ -1,12 +1,11 @@
 import torch
-from caduceus.caduceus import CaduceusTokenizer, KMerTokenizer
+from caduceus.caduceus import CaduceusTokenizer, KMerTokenizer, ByteTokenizer
 from hnet.hnet.models.mixer_seq import HNetForCausalLM
 from hnet.hnet.models.config_hnet import (
     AttnConfig,
     SSMConfig,
     HNetConfig,
 )
-from hnet.hnet.utils.tokenizers import ByteTokenizer
 from omegaconf import OmegaConf
 from wrappers.composer import ComposerWrapper
 
@@ -18,14 +17,21 @@ def build_model(**model_config):
     # ssm_cfg = SSMConfig(**model_config.get("ssm_cfg"))
     ignore_keys = ["max_seq_len", "mlm", "default_target_ratio", "_modelstr_"]
     use_kmer = model_config.get("use_kmer", False)
-    if use_kmer:
+    tok_type = model_config.get("tokenizer", None)
+    tokenizer = None
+    if tok_type is None or tok_type == "dna":
+        tokenizer = CaduceusTokenizer(model_max_length=model_config["max_seq_len"])
+    elif tok_type == "byte":
+        tokenizer = ByteTokenizer(model_max_length=model_config["max_seq_len"])
+    elif tok_type == "kmer":
         tokenizer = KMerTokenizer(
             k=model_config["k"], model_max_length=model_config["max_seq_len"]
         )
-        if model_config["vocab_size"] < len(tokenizer):
-            model_config["vocab_size"] = len(tokenizer)
     else:
-        tokenizer = CaduceusTokenizer(model_max_length=model_config["max_seq_len"])
+        raise Exception(f"Unkown tokenizer type {tok_type}")
+    if model_config["vocab_size"] < len(tokenizer):
+        model_config["vocab_size"] = len(tokenizer)
+
     print(model_config)
     hnet_cfg = HNetConfig(
         **{x: v for x, v in model_config.items() if x not in ignore_keys},
